88b6791dbb026ecf49d65d96a6cb2172e4a68ee2
RaviLochan <48204237+Ravilochan@users.noreply.github.com>
2020-12-14 22:17:42 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
   var firebaseConfig = {
-    apiKey: "AIzaSyDrA-3OuAkhPn1ARGR0vm10wzjcb3dJQQc",
+    apiKey: "#",
     authDomain: "slack-e07ff.firebaseapp.com",
     databaseURL: "https://slack-e07ff.firebaseio.com",
     projectId: "slack-e07ff",
     storageBucket: "slack-e07ff.appspot.com",
-    messagingSenderId: "465702697856",
-    appId: "1:465702697856:web:eed03406d8345f6fa31f65",
-    measurementId: "G-BW7BSVDH5H"
+    messagingSenderId: "#",
+    appId: "#",
+    measurementId: "#"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
